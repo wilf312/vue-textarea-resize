13843c56e85ccfa49b818c59ae916ffdb8386f75
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-12-01 12:58:55 +0900

    init

--- src/my-component.vue
+++ src/my-component.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>{{message}}</div>
+  <textarea v-autosize :value="value" @input="onInput" :style="style" />
 </template>
 
 <script>
+import autosize from "v-autosize";
+
 export default {
-  data() {
-    return {
-      message: "message dayo"
-    };
+  directives: {
+    autosize
+  },
+  props: {
+    value: {
+      required: true,
+      type: String
+    },
+    minHeight: {
+      type: String | Number,
+      default: 0
+    },
+    maxHeight: {
+      type: String | Number,
+      default: 0
+    }
+  },
+  methods: {
+    onInput(ev) {
+      this.$emit("input", ev.target.value);
+    }
+  },
+  computed: {
+    style() {
+      return {
+        minHeight: this.minHeight ? `${this.minHeight}px` : "",
+        maxHeight: this.maxHeight ? `${this.maxHeight}px` : ""
+      };
+    }
   }
 };
 </script>
