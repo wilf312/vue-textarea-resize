2e9714515b785c3ff1f322dcfc602c8688c825c8
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-12-01 12:42:49 +0900

    fix init

---
+++ build/rollup.config.js
@@ -0,0 +1,18 @@
+import commonjs from 'rollup-plugin-commonjs'; // CommonJS モジュールを ES6 に変換
+import vue from 'rollup-plugin-vue'; // .vue 単一ファイルコンポーネントを取得
+import buble from 'rollup-plugin-buble'; // 適切にブラウザをサポートするトランスパイラおよびポリフィル
+export default {
+  input: 'src/wrapper.js', // Path relative to package.json
+  output: {
+    name: 'MyComponent',
+    exports: 'named',
+  },
+  plugins: [
+    commonjs(),
+    vue({
+      css: true, // css を <style> タグとして注入
+      compileTemplate: true, // 明示的にテンプレートを描画関数に変換
+    }),
+    buble(), // ES5 へトランスパイルする
+  ],
+};
